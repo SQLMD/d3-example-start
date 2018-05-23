860b70bf44e137c90284adf8ffddc27f0945535b
Mike Donnelly <sqldonnelly@gmail.com>
2018-05-23 23:41:29 +0900

    Doughnut changes

--- script/doughnut.js
+++ script/doughnut.js
@@ -2,30 +2,40 @@ const getIPs = async (filePath, minValue) => {
   const width = 300;
   const height = 300;
   try {
-    const d = await d3.json(filePath);
-    const array = d.account_activity.map(item => item.ip_address);
-    const ips = countBy(array)
+    const importAccountActivity = await d3.json(filePath);
+
+    const activities = importAccountActivity.account_activity.map(
+      activity => activity.ip_address
+    );
+
+    const activityIPs = countBy(activities)
       .filter(ip => ip.value > minValue)
       .sort((ip1, ip2) => ip2.value - ip1.value);
-    const dataset = ips.map(ip => ip.value);
 
+    console.log("Activity IP Addresses with Counts:", activityIPs);
+    const countValues = activityIPs.map(ip => ip.value);
+
+    console.log("Count Values", countValues);
     const outerRadius = width / 2;
     const innerRadius = width / 3;
-
-    const pie = d3.pie();
-
     const arc = d3
       .arc()
       .innerRadius(innerRadius)
       .outerRadius(outerRadius);
 
-    console.log(dataset);
-    console.log(pie(dataset));
+    const pie = d3.pie();
+
+    console.log("Count Values with Pie", pie(countValues));
 
     //Easy colors accessible via a 10-step ordinal scale
     const color = d3.scaleOrdinal(d3.schemeCategory10);
 
     //Create SVG element
+    const svg = d3
+      .select("pie")
+      .append("svg")
+      .attr("width", width)
+      .attr("height", height);
 
     //Set up groups
 
@@ -33,15 +43,7 @@ const getIPs = async (filePath, minValue) => {
 
     //Labels
 
-    //Key
-    d3
-      .select("pieL")
-      .selectAll("div")
-      .data(ips)
-      .enter()
-      .append("div")
-      .text(d => d.id + ": " + d.value)
-      .attr("style", "font-size:30px");
+    //add labels
   } catch (err) {
     console.log(err);
   }
